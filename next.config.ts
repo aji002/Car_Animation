92312e665e4abe32509e29767c683069d80045f0
Ajil <ajila858@gmail.com>
2026-02-24 12:46:00 +0530

    Update next config for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   output: "export",
+  basePath: "/Car_Animation",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
