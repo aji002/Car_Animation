5d4e46cf6213440cb3da672635ddab3e31202139
Ajil <ajila858@gmail.com>
2026-02-24 13:07:22 +0530

    Updates

--- _next/static/7Cd4dOybTuXL2fnzAoJ0C/_buildManifest.js
+++ _next/static/7Cd4dOybTuXL2fnzAoJ0C/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Car_Animation//_next/:path+",
+        "destination": "/Car_Animation/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
